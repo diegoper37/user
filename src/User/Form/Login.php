3a5b6b65310c74491f9da9fc727f301c0cab5402
Diego Pimentel <diegoper37@hotmail.com>
2013-08-07 09:45:02 -0300

    Forms de login e filtro

---
+++ src/User/Form/Login.php
@@ -0,0 +1,58 @@
+<?php
+namespace User\Form; 
+
+use Zend\Captcha; 
+use Zend\Form\Element; 
+use Zend\Form\Form; 
+
+class Login extends Form 
+
+{ 
+    public function __construct($name = null) 
+    { 
+        parent::__construct(''); 
+        
+        $this->setAttribute('method', 'post'); 
+        
+        $this->add(array( 
+            'name' => 'email', 
+            'type' => 'Zend\Form\Element\Email', 
+            'attributes' => array( 
+                'placeholder' => 'Email Address...', 
+                'required' => 'required', 
+            ), 
+            'options' => array( 
+                'label' => 'Email', 
+            ), 
+        )); 
+ 
+        $this->add(array( 
+            'name' => 'password', 
+            'type' => 'Zend\Form\Element\Password', 
+            'attributes' => array( 
+                'placeholder' => 'Password Here...', 
+                'required' => 'required', 
+            ), 
+            'options' => array( 
+                'label' => 'Password', 
+            ), 
+        )); 
+ 
+        $this->add(array( 
+            'name' => 'password_verify', 
+            'type' => 'Zend\Form\Element\Password', 
+            'attributes' => array( 
+                'placeholder' => 'Verify Password Here...', 
+                'required' => 'required', 
+            ), 
+            'options' => array( 
+                'label' => 'Verify Password', 
+            ), 
+        )); 
+ 
+        $this->add(array( 
+            'name' => 'csrf', 
+            'type' => 'Zend\Form\Element\Csrf', 
+        ));        
+    } 
+} 
